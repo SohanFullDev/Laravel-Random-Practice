0e21a68c776761e5f6cb2c756aab80a7e9d4388f
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-09-24 16:48:37 +0600

    1_cities view,route,controller,model,migrations

---
+++ resources/views/cities/cities_view.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ALL CITIES</title>
+    <style>
+        table,tr,th,td{
+            border: 1px solid black;
+            border-collapse: collapse;
+            padding: 3px;
+        }
+
+    </style>
+</head>
+<body>
+    <h1>All Cities</h1>
+    <table>
+            <tr>
+                <th>Name</th>
+                <th>Abbr</th>
+                <th>Capital</th>
+                <th>Established</th>
+                <th>Population</th>
+                <th>Action</th>
+            </tr>
+            <tr>
+                <td>Alabama</td>
+                <td>AL</td>
+                <td>Montgomery</td>
+                <td>Dec 14, 1819</td>
+                <td>4,874,747</td>
+                <td>
+                    <a href="">Edit</a>
+                    <a href="">Delete</a>
+
+                </td>
+            </tr>
+
+
+    </table>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CityController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,4 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
+Route::get('/cities', [CityController::class, 'CitiesView']);
